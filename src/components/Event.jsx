fb842931ae36a8eb39288276b06ed124ae2085cd
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-02-23 16:06:42 -0800

    Add basic background colors, add form input, add hard-coded event

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 function Event() {
+  const eventStyles = {
+    backgroundColor: '#E88222'
+  }
   return(
-    <div>Event works!</div>
+    <div style={eventStyles}>
+      <h1>Pedalpalooza</h1>
+      <h4>Date: June </h4>
+      <h4>Location: Rides around Portland,OR </h4>
+      <h4>Price: Free </h4>
+      <h4>Notes:</h4>
+      <p>Visit <a href='http://www.shift2bikes.org/'>Pedalpalooza website</a> for more information. Find out when Get Lost, Loud and Lit, and Bowie vs. Prince rides are this year.</p>
+    </div>
   );
 }
 
--- src/components/NewEventForm.jsx
+++ src/components/NewEventForm.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
 function NewEventForm() {
+  const newFormStyles = {
+    backgroundColor: '#529967'
+  }
+
   return(
     <div>
-      NewEventForm works
+      <form style={newFormStyles}>
+        <label>Name: </label>
+        <input
+          type='text'
+          id='name'/>
+        <label>Date: </label>
+        <input
+          type='text'
+          id='date'/>
+        <label>Location: </label>
+        <input
+          type='text'
+          id='location'/>
+        <label>Price: </label>
+        <input
+          type='text'
+          id='price'/>
+        <label>Notes: </label>
+        <textarea
+          type='text'
+          id='notes'/>
+      </form>
     </div>
   );
 }
